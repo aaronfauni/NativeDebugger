d53d4a800c0b277911dd484db1eb8e6ad1e37142
aaronfauni <aacfauni@ucsc.edu>
2025-06-23 21:29:40 +0000

    Implemented processes attachment w/ fork() and exec()

---
+++ sdb/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/aaronfauni/github/NativeDebugger/sdb
+# Build directory: /home/aaronfauni/github/NativeDebugger/sdb/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tools")
+subdirs("test")
---
+++ sdb/build/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aaronfauni/github/NativeDebugger/sdb/test
+# Build directory: /home/aaronfauni/github/NativeDebugger/sdb/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
